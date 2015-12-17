f453a8e544cb4052feb56f4cf77ba79122aeedf8
Shane Canon <scanon@lbl.gov>
2015-12-16 17:31:04 -0800

    Fixes to client test and start server

--- test.py
+++ test.py
@@ -6,6 +6,16 @@ url=sys.argv[1]
 
 p=data_api2.data_api2Client.data_api2(url=url)
 
-taxon=p.get_taxon("ReferenceGenomeAnnotations/kb|g.207118")
+obj_name="ReferenceGenomeAnnotations/kb|g.140057"
+feature='kb|g.140057.CDS.2901'
+gene='kb|g.140057.locus.2922'
+mrna='kb|g.140057.mRNA.2840'
+taxon= u'1779/523209/1'
+assembly='1837/56/1'
 
-print taxon
+print p.get_taxon(obj_name)
+print p.get_assembly(obj_name)
+
+print p.get_features(obj_name,[feature])
+print p.get_cds_by_mrna(obj_name,[mrna])
+
